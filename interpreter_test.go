b2b1f735385174af42be46821a61a6ae27ced031
Riley <o0lit3@gmail.com>
2021-05-30 17:18:42 -0500

    Moved test files

--- interpreter_test.go
+++ interpreter_test.go
@@ -3,127 +3,127 @@ import "testing"
 
 // Member ops
 func TestMember(t *testing.T) {
-    test(t, "ops/member.dish")
+    test(t, "tests/member.dish")
 }
 
 // Unary ops
 func TestNot(t *testing.T) {
-    test(t, "ops/not.dish")
+    test(t, "tests/not.dish")
 }
 
 func TestInverse(t *testing.T) {
-    test(t, "ops/invert.dish")
+    test(t, "tests/invert.dish")
 }
 
 func TestProduct(t *testing.T) {
-    test(t, "ops/product.dish")
+    test(t, "tests/product.dish")
 }
 
 func TestItemize(t *testing.T) {
-    test(t, "ops/itemize.dish")
+    test(t, "tests/itemize.dish")
 }
 
 func TestSum(t *testing.T) {
-    test(t, "ops/sum.dish")
+    test(t, "tests/sum.dish")
 }
 
 func TestNegate(t *testing.T) {
-    test(t, "ops/negate.dish")
+    test(t, "tests/negate.dish")
 }
 
 func TestStringify(t *testing.T) {
-    test(t, "ops/stringify.dish")
+    test(t, "tests/stringify.dish")
 }
 
 func TestMin(t *testing.T) {
-    test(t, "ops/min.dish")
+    test(t, "tests/min.dish")
 }
 
 func TestAverage(t *testing.T) {
-    test(t, "ops/average.dish")
+    test(t, "tests/average.dish")
 }
 
 func TestMax(t *testing.T) {
-    test(t, "ops/max.dish")
+    test(t, "tests/max.dish")
 }
 
 func TestUnique(t *testing.T) {
-    test(t, "ops/uniq.dish")
+    test(t, "tests/uniq.dish")
 }
 
 func TestLength(t *testing.T) {
-    test(t, "ops/length.dish")
+    test(t, "tests/length.dish")
 }
 
 func TestIncrement(t *testing.T) {
-    test(t, "ops/increment.dish")
+    test(t, "tests/increment.dish")
 }
 
 func TestDecrement(t *testing.T) {
-    test(t, "ops/decrement.dish")
+    test(t, "tests/decrement.dish")
 }
 
 // Binary ops
 func TestSwitch(t *testing.T) {
-    test(t, "ops/switch.dish")
+    test(t, "tests/switch.dish")
 }
 
 func TestFind(t *testing.T) {
-    test(t, "ops/find.dish")
+    test(t, "tests/find.dish")
 }
 
 func TestPower(t *testing.T) {
-    test(t, "ops/power.dish")
+    test(t, "tests/power.dish")
 }
 
 func TestMultiply(t *testing.T) {
-    test(t, "ops/multiply.dish")
+    test(t, "tests/multiply.dish")
 }
 
 func TestDivide(t *testing.T) {
-    test(t, "ops/divide.dish")
+    test(t, "tests/divide.dish")
 }
 
 func TestPlus(t *testing.T) {
-    test(t, "ops/add.dish")
+    test(t, "tests/add.dish")
 }
 
 func TestSubtract(t *testing.T) {
-    test(t, "ops/subtract.dish")
+    test(t, "tests/subtract.dish")
 }
 
 func TestJoin(t *testing.T) {
-    test(t, "ops/join.dish")
+    test(t, "tests/join.dish")
 }
 
 func TestBelow(t *testing.T) {
-    test(t, "ops/below.dish")
+    test(t, "tests/below.dish")
 }
 
 func TestUnder(t *testing.T) {
-    test(t, "ops/under.dish")
+    test(t, "tests/under.dish")
 }
 
 func TestAbove(t *testing.T) {
-    test(t, "ops/above.dish")
+    test(t, "tests/above.dish")
 }
 
 func TestOver(t *testing.T) {
-    test(t, "ops/over.dish")
+    test(t, "tests/over.dish")
 }
 
 func TestEquals(t *testing.T) {
-    test(t, "ops/equals.dish")
+    test(t, "tests/equals.dish")
 }
 
 func TestAnd(t *testing.T) {
-    test(t, "ops/and.dish")
+    test(t, "tests/and.dish")
 }
 
 func TestOr(t *testing.T) {
-    test(t, "ops/or.dish")
+    test(t, "tests/or.dish")
 }
 
 func TestRange(t *testing.T) {
-    test(t, "ops/range.dish")
+    test(t, "tests/range.dish")
 }
